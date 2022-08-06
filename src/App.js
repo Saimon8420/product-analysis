bd7dc9054b081e7d005487100fa1c5b7ba87f009
Md. Latiful Kabir <latifulkabir@gmail.com>
2022-08-07 01:30:34 +0600

    route seted for all component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Reviews from './components/Reviews/Reviews';
+import Dashboard from './components/Dashboard/Dashboard';
+import NotFound from './components/NotFound/NotFound';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
+        <Route path='/' element={<Home />}></Route>
+
+        <Route path='/home' element={<Home />}></Route>
+
+        <Route path='/review' element={<Reviews />}></Route>
+
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <Link to='/home'>Home</Link>
+                <Link to='/review'>Review</Link>
+                <Link to='/dashboard'>Dashboard</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
